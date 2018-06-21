fd7dff9ac7eeb0bde7780fb10da9122486d98eb5
ProteanIT <39917867+ProteanIT@users.noreply.github.com>
2018-06-21 12:00:30 +0100

    Update ModifyGarminTimestamps.ps1
    Added comments

--- ModifyGarminTimestamps.ps1
+++ ModifyGarminTimestamps.ps1
@@ -1,10 +1,24 @@
-﻿[xml] $xml = Get-Content C:\Temp\activity_2790529943.tcx
+﻿
+<#
+Head over to your connect account and export your
+activity to a tcx file. 
 
+This should probably parameterised at somepoint.
+#>
+
+[xml] $xml = Get-Content C:\Temp\activity_2790529943.tcx
+
+#These two times will give us the timespan that needs to be removed from the subsequent trackpoints
 $LastGoodTime = [datetime]"2018-06-20T07:22:58.000Z"
 
 $FirstBadTime = [datetime]"2019-04-07T00:14:04.000Z"
 
 $ts = New-TimeSpan -Start $LastGoodTime -End $FirstBadTime
+
+<#
+Get all the time nodes in the tcx file and if they are more advanced 
+#than the last good time, subtract the interval from it.
+#>
 
 $xml.TrainingCenterDatabase.Activities.Activity.Lap.Track.Trackpoint.ChildNodes| where {$_.name -eq "Time"} |% {
 
